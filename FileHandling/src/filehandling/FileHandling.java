e455a9a524109bff693674da254ef4e4b9b990b4
Sohamnimbalkar07 <131379333+Sohamnimbalkar07@users.noreply.github.com>
2024-01-21 22:20:54 +0530

    Add files via upload

---
+++ FileHandling/src/filehandling/FileHandling.java
@@ -0,0 +1,35 @@
+package filehandling;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class FileHandling {
+
+	public static void main(String[] args) {
+		
+		System.out.println("enter file names in two different lines");
+		
+		try(
+			Scanner sc = new Scanner(System.in);
+			BufferedReader br = new BufferedReader(new FileReader(sc.nextLine()));
+			PrintWriter pw = new PrintWriter(new FileWriter(sc.nextLine()));
+			)
+		{
+			String s = null;
+			while((s= br.readLine()) != null)
+			{
+				pw.println(s);
+			}
+			System.out.println("File copy done!");
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+
+	}
+
+}
