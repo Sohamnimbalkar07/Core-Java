c73cdbf8a8aaf880c3f0dd10eb789a463ad9d6a3
Soham Nimbalkar <131379333+Sohamnimbalkar07@users.noreply.github.com>
2024-10-27 16:53:17 +0530

    Delete day8-data directory

--- day8-data/eclipse-projects/day8.2/src/custom_exceptions/SpeedOutOfRangeException.java
+++
@@ -1,8 +0,0 @@
-package custom_exceptions;
-
-@SuppressWarnings("serial") // to tell javac to suppress : serialization related warnings (covered later)
-public class SpeedOutOfRangeException extends Exception {
-	public SpeedOutOfRangeException(String errMesg) {
-		super(errMesg);
-	}
-}
