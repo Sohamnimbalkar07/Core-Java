c289ace7bbaa49cdb23c4342d4b6f1c9bf80924f
Soham Nimbalkar <131379333+Sohamnimbalkar07@users.noreply.github.com>
2024-10-27 16:52:51 +0530

    Delete day6-data directory

--- day6-data/eclipse-projects/day6.2/src/p2/Printable.java
+++
@@ -1,9 +0,0 @@
-package p2;
-
-public interface Printable {
-	// javac adds : public , static , final
-	double TEST_DATA = 123.45;
-
-//javac adds : public , abstract => specification
-	void print(String mesg);
-}
