5b521ff9a363ac37ca3d0293fad1b5975276ccc3
Soham Nimbalkar <131379333+Sohamnimbalkar07@users.noreply.github.com>
2024-10-27 16:52:23 +0530

    Delete day4-data directory

--- day4-data/eclipse-projects/day4.2/src/inheritance/A.java
+++
@@ -1,9 +0,0 @@
-package inheritance;
-
-public class A //extends Object
-{
-	A() {
-	//	super(); javac add super() --> Object cls's constr
-		System.out.println("1");
-	}
-}
--- day4-data/eclipse-projects/day4.2/src/inheritance/C.java
+++
@@ -1,8 +0,0 @@
-package inheritance;
-
-public class C extends B{
-	C() {
-//		super();//javac implicitly add super() => a call to immediate super's class's ctor.
-		System.out.println("3");
-	}
-}
